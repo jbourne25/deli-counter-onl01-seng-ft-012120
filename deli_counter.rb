b4e7e1af4b5764fb626f06f9b74ba4587355e256
Jeremy Bourne <jbourne25@yahoo.com>
2020-01-24 18:23:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
 katz_deli = []
 
 def line(katz_deli)
-  new_array = []
+ # new_array = []
 if katz_deli.length > 0
-  katz_deli.each{ |index, person| new_array << "The line is currently: #{index}. #{person}" }
+  katz_deli.collect{ |index, person| "The line is currently: #{index}. #{person}" }
   else 
     puts "The line is currently empty."
   end 
 end 
 
 def take_a_number(katz_deli, new_customer)
-  
+end   
 
 
     
